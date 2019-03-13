844536926067801dad2cf2f79ee2791077c72e97
changjiale <1056430312@qq.com>
2019-03-13 09:54:02 +0800

    猫眼电影爬取

---
+++ 项目2-猫眼电影爬取/maoyan.py
@@ -0,0 +1,47 @@
+import json
+import requests
+from requests.exceptions import RequestException
+import re
+import time
+
+#判断url是否请求成功
+def get_one_page(url):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.text
+        return None
+    except RequestException:
+        return None
+#正则提取关键信息
+def parse_one_page(html):
+    pattern = re.compile('<dd>.*?board-index.*?>(\d+)</i>.*?data-src="(.*?)".*?name"><a'
+                         + '.*?>(.*?)</a>.*?star">(.*?)</p>.*?releasetime">(.*?)</p>'
+                         + '.*?integer">(.*?)</i>.*?fraction">(.*?)</i>.*?</dd>', re.S)
+    items = re.findall(pattern, html)
+    for item in items:
+        yield {
+            'index': item[0],
+            'image': item[1],
+            'title': item[2],
+            'actor': item[3].strip()[3:],
+            'time': item[4].strip()[5:],
+            'score': item[5] + item[6]
+        }
+#写入文件
+def write_to_file(content):
+    with open('result.txt', 'a', encoding='utf-8') as f:
+        f.write(json.dumps(content, ensure_ascii=False) + '\n')
+ 
+#构造URL
+def main(offset):
+    url = 'http://maoyan.com/board/4?offset=' + str(offset)
+    html = get_one_page(url)
+    for item in parse_one_page(html):
+        print(item)
+        write_to_file(item)
+ 
+if __name__ == '__main__':
+    for i in range(2):
+        main(offset=i * 2)
+        time.sleep(1)
