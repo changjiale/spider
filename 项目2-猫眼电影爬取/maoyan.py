6ef407459a248491879ab682ec295eb13860b57b
changjiale <1056430312@qq.com>
2019-03-14 16:51:46 +0800

    爬取猫眼电影

--- 项目2-猫眼电影爬取/maoyan.py
+++ 项目2-猫眼电影爬取/maoyan.py
@@ -42,6 +42,6 @@ def main(offset):
         write_to_file(item)
  
 if __name__ == '__main__':
-    for i in range(2):
-        main(offset=i * 2)
+    for i in range(10):
+        main(offset=i * 10)
         time.sleep(1)
