e45a091438bd72246024921652cfd76cc220ae10
changjiale <1056430312@qq.com>
2019-03-19 09:55:29 +0800

    获取代理66的ip

--- 项目6-构建代理池/proxypool/crawler.py
+++ 项目6-构建代理池/proxypool/crawler.py
@@ -1,9 +1,49 @@
 import json
 import re
-
+from pyquery import PyQuery as pq
+from .utils import get_page
 class ProxyMetaclass(type):
     def __new__(cls, name, bases, attrs):
         count = 0
+        attrs['__CrawlFunc__'] = []
+        for k, v in attrs.items():
+            if 'crawl_' in k:
+                attrs['__CrawlFunc__'].append(k)
+                count += 1
+        attrs['__CrawlFunc__'] = count
+        return type.__new__(cls, name, bases, attrs)
+
+class Crawler(object, metaclass=ProxyMetaclass):
+    def get_proxies(self, callback):
+        '''
+        从类中其他方法获取代理，并存入集合中
+        :param callback:  回调函数（函数名称）
+        :return:代理列表
+        '''
+        proxies = []
+        for proxy in eval('self.{}()',format(callback)):
+            print('代理获取成功', proxy)
+            proxies.append(proxy)
+        return proxies
+
+    def crawl_daili66(self, page_cout=4):
+        """
+        获取代理66
+        :param page_count: 页码
+        :return: 代理
+        """
+        start_url = 'http://www.66ip.cn/{}.html'
+        urls = [start_url.format(page) for page in range(1, page_cout + 1)]
+        for url in urls:
+            print('Crawling',url)
+            html = get_page(url)
+            if html:
+                doc = pq(html)
+                trs = doc('.containerbox table tr:gt(0)').items()
+                for tr in trs:
+                    ip = tr.find('td:nth-child(1)').text()
+                    port = tr.find('td:nth-child(2)').text()
+                    yield ':'.join([ip, port])
 
 class PoolEmptyError(Exception):
 
