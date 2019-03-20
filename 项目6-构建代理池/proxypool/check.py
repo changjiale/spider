089eac5229b092d9aeedb8860e1f0f3aec4aee14
changjiale <1056430312@qq.com>
2019-03-20 13:46:18 +0800

    测试代理池可用代理

--- 项目6-构建代理池/proxypool/check.py
+++ 项目6-构建代理池/proxypool/check.py
@@ -43,3 +43,28 @@ class Tester(object):
                 self.redis.decrease(proxy)
                 print('代理请求失败', proxy)
 
+    def run(self):
+        '''
+        遍历代理池，设置批量测试最大100个
+        :return:
+        '''
+        print('测试器开始运行')
+        try:
+            count = self.redis.count()
+            print('当前剩余', count, '个代理')
+            for i in range(0, count, 100):
+                start = i
+                stop = min(i+100, count)
+                print('正在测试第', start + 1, '-', stop, '个代理')
+                test_proxies = self.redis.batch(start, stop)
+                loop = asyncio.get_event_loop()
+                tasks = [self.test_single_proxy(proxy) for proxy in test_proxies]
+                loop.run_until_complete(asyncio.wait(tasks))
+                sys.stdout.flush()
+                time.sleep(5)
+        except Exception as e:
+            print('测试发生错误', e.args)
+
+if __name__ == '__main__':
+    Tester().run()
+
